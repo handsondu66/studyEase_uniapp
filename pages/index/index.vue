ee14304a4995c126d2684cc234d8a87922e04298
jlDu1999 <2868693053@qq.com>
2023-02-10 23:17:50 +0800

    项目框架

---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="container">
+		
+		<view class="intro" @click="test">测试</view>
+		<text class="intro">详见：</text>
+		<uni-link :href="href" :text="href"></uni-link>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+			async test(){
+				console.log("test______")
+				const res = await uni.$http.get('/books');
+				console.log(res)
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
